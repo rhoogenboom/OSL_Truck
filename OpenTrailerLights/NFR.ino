86e029349261ddc682dfcf8d4c64d444bfe9d299
Richard Hoogenboom <richard.hoogenboom@belleron.net>
2018-02-22 21:06:31 +0100

    Enabled light control and debug code

--- OpenTrailerLights/NFR.ino
+++ OpenTrailerLights/NFR.ino
@@ -5,8 +5,17 @@ void receiveControllerInfo() {
   
     if (servoPosition != 0) {
         updateServoPositions(limitToMaxPositionsServo(servoPosition));
-    }       
+    } 
+
+    SetLights();
   }
-  
+  //printPacketContents();
+}
+
+void printPacketContents() {
+  Serial.print(F("Controller: ")); Serial.println(packet.controller3);
+  for (int i=0;i<12;i++) {
+    Serial.print(F("Light:  ")); Serial.println(packet.lights[i].state);
+  }
 }
 
