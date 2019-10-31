53aaa95085808b9d97b5802552aeeebe1ac213da
Richard Hoogenboom <rbhoogenboom@gmail.com>
2019-10-31 23:41:19 +0100

    Reinit rom

--- OpenTruckLights/Variables.h
+++ OpenTruckLights/Variables.h
@@ -3,7 +3,7 @@ bool setupActive = false; //defines whether we are in setup mode or not
 int buttonState = 0; //state of setupbutton default is open
 
 // EEPROM
-const long EEPROM_Init = 0xA03; // Change this any time the EEPROM content changes
+const long EEPROM_Init = 0xA04; // Change this any time the EEPROM content changes
 
 struct __eeprom_data { //  structure that maps all of the data we are storing in EEPROM
   long E_InitNum;      // Number that indicates if EEPROM values have ever been initialized
